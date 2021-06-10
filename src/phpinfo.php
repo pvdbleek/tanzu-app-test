1cda9c8d97be5cc8d79970aed1b1cd886f87ad56
Drouard Ludovic <ludovic.drouard@orange.com>
2021-06-10 20:13:31 +0000

    Add more info in PHPinfo

--- src/phpinfo.php
+++ src/phpinfo.php
@@ -1,3 +1,8 @@
 <?php
+  phpinfo(INFO_GENERAL);
+  phpinfo(INFO_CONFIGURATION);
   phpinfo(INFO_MODULES);
+  // /!\ High Security Issues
+  //phpinfo(INFO_VARIABLES);
+  //phpinfo(INFO_ENVIRONMENT);
 ?>
